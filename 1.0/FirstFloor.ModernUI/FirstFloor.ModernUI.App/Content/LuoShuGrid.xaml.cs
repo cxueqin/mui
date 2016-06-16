116bbd7d4ba51be7f2e9b68a5167802b35cdee39
xueqin <chenxq@star-net.cn>
2016-06-16 14:26:46 +0800

    LoremIpsumGrid重命名为LuoShuGrid

--- 1.0/FirstFloor.ModernUI/FirstFloor.ModernUI.App/Content/LuoShuGrid.xaml.cs
+++ 1.0/FirstFloor.ModernUI/FirstFloor.ModernUI.App/Content/LuoShuGrid.xaml.cs
@@ -19,9 +19,9 @@ namespace FirstFloor.ModernUI.App.Content
     /// <summary>
     /// Interaction logic for LoremIpsumSplit.xaml
     /// </summary>
-    public partial class LoremIpsumGrid : UserControl
+    public partial class LuoShuGrid : UserControl
     {
-        public LoremIpsumGrid()
+        public LuoShuGrid()
         {
             InitializeComponent();
         }
